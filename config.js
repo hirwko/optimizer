4da05b6c0d0e9818a96411991b064a1f99265d82
Cloudie <158478884+hirwko@users.noreply.github.com>
2025-03-02 16:36:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
-
-
 module.exports = {
   TOKEN: "",
   language: "ja",
   ownerID: ["1106711955563098152", ""], 
-  mongodbUri : "mongodb+srv://tatsumihirako:Revel001@discordbot.wdmjg.mongodb.net/?retryWrites=true&w=majority",
+  mongodbUri: "mongodb+srv://tatsumihirako:Revel001@discordbot.wdmjg.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "chilling", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "music", 
+  activityType: "LISTENING",  // Available activity types: LISTENING, PLAYING
   SupportServer: "https://discord.gg/GSsbGwV8vt",
   embedTimeout: 5, 
   errorLog: "", 
@@ -21,5 +19,6 @@ module.exports = {
       port:  5017,
       secure: false
     }
-  ]
+  ],
+  prefix: "-"  // Add the prefix here
 }
