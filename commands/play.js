648bfde923fa349bf884c3ae941f8261a29ea781
Cloudie <158478884+hirwko@users.noreply.github.com>
2025-03-02 17:48:09 +0900

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,25 +1,23 @@
-const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const config = require('../config.js');
 const musicIcons = require('../UI/icons/musicicons.js');
-const queueNames = [];
-const requesters = new Map();
 
-async function play(client, interaction, lang) {
+async function play(client, message, args) {
     try {
-        const query = interaction.options.getString('name');
+        const query = args.join(" "); // Get the song name or URL from args
 
-        if (!interaction.member.voice.channelId) {
+        if (!message.member.voice.channelId) {
             const embed = new EmbedBuilder()
                 .setColor('#ff0000')
                 .setAuthor({ 
-                    name: lang.play.embed.error, 
+                    name: 'Error', 
                     iconURL: musicIcons.alertIcon,
                     url: config.SupportServer
                 })
-                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon })
-                .setDescription(lang.play.embed.noVoiceChannel);
+                .setFooter({ text: 'Footer', iconURL: musicIcons.heartIcon })
+                .setDescription('You need to join a voice channel first!');
 
-            await interaction.reply({ embeds: [embed], ephemeral: true });
+            await message.reply({ embeds: [embed] });
             return;
         }
 
@@ -27,114 +25,86 @@ async function play(client, interaction, lang) {
             const embed = new EmbedBuilder()
                 .setColor('#ff0000')
                 .setAuthor({ 
-                    name: lang.play.embed.error,
+                    name: 'Error',
                     iconURL: musicIcons.alertIcon,
                     url: config.SupportServer
                 })
-                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon })
-                .setDescription(lang.play.embed.noLavalinkNodes);
+                .setFooter({ text: 'Footer', iconURL: musicIcons.heartIcon })
+                .setDescription('No Lavalink nodes available.');
 
-            await interaction.reply({ embeds: [embed], ephemeral: true });
+            await message.reply({ embeds: [embed] });
             return;
         }
 
         const player = client.riffy.createConnection({
-            guildId: interaction.guildId,
-            voiceChannel: interaction.member.voice.channelId,
-            textChannel: interaction.channelId,
+            guildId: message.guildId,
+            voiceChannel: message.member.voice.channelId,
+            textChannel: message.channelId,
             deaf: true
         });
 
-        await interaction.deferReply();
-
-        const resolve = await client.riffy.resolve({ query: query, requester: interaction.user.username });
+        const resolve = await client.riffy.resolve({ query: query, requester: message.author.username });
         if (!resolve || typeof resolve !== 'object') {
             throw new TypeError('Resolve response is not an object');
         }
 
-        const { loadType, tracks, playlistInfo } = resolve;
+        const { loadType, tracks } = resolve;
 
         if (!Array.isArray(tracks)) {
             throw new TypeError('Expected tracks to be an array');
         }
 
-        if (loadType === 'playlist') {
-            for (const track of tracks) {
-                track.info.requester = interaction.user.username;
-                player.queue.add(track);
-                queueNames.push(`[${track.info.title} - ${track.info.author}](${track.info.uri})`);
-                requesters.set(track.info.uri, interaction.user.username);
-            }
-
-            if (!player.playing && !player.paused) player.play();
-
-        } else if (loadType === 'search' || loadType === 'track') {
+        if (loadType === 'track' || loadType === 'search') {
             const track = tracks.shift();
-            track.info.requester = interaction.user.username;
+            track.info.requester = message.author.username;
 
             player.queue.add(track);
-            queueNames.push(`[${track.info.title} - ${track.info.author}](${track.info.uri})`);
-            requesters.set(track.info.uri, interaction.user.username);
 
             if (!player.playing && !player.paused) player.play();
+
+            const successEmbed = new EmbedBuilder()
+                .setColor(config.embedColor)
+                .setAuthor({
+                    name: 'Track Added',
+                    iconURL: musicIcons.beats2Icon,
+                    url: config.SupportServer
+                })
+                .setDescription(`Now playing: [${track.info.title}](https://www.youtube.com/watch?v=${track.info.uri})`)
+                .setFooter({ text: 'Footer', iconURL: musicIcons.heartIcon });
+
+            await message.reply({ embeds: [successEmbed] });
         } else {
             const errorEmbed = new EmbedBuilder()
-                .setColor(config.embedColor)
+                .setColor('#ff0000')
                 .setAuthor({ 
-                    name: lang.play.embed.error,
+                    name: 'Error',
                     iconURL: musicIcons.alertIcon,
                     url: config.SupportServer
                 })
-                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon })
-                .setDescription(lang.play.embed.noResults);
+                .setFooter({ text: 'Footer', iconURL: musicIcons.heartIcon })
+                .setDescription('No results found for your query.');
 
-            await interaction.editReply({ embeds: [errorEmbed] });
-            return;
+            await message.reply({ embeds: [errorEmbed] });
         }
 
-        const randomEmbed = new EmbedBuilder()
-            .setColor(config.embedColor)
-            .setAuthor({
-                name: lang.play.embed.requestUpdated,
-                iconURL: musicIcons.beats2Icon,
-                url: config.SupportServer
-            })
-            .setDescription(lang.play.embed.successProcessed)
-            .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon });
-
-        await interaction.followUp({ embeds: [randomEmbed] });
-
     } catch (error) {
-        console.error('Error processing play command:', error);
+        console.error(error);
         const errorEmbed = new EmbedBuilder()
             .setColor('#ff0000')
             .setAuthor({ 
-                name: lang.play.embed.error,
+                name: 'Error',
                 iconURL: musicIcons.alertIcon,
                 url: config.SupportServer
             })
-            .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon })
-            .setDescription(lang.play.embed.errorProcessing);
+            .setFooter({ text: 'made by hirako!', iconURL: musicIcons.heartIcon })
+            .setDescription('There was an error processing your request.');
 
-        if (interaction.deferred || interaction.replied) {
-            await interaction.editReply({ embeds: [errorEmbed] });
-        } else {
-            await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
-        }
+        await message.reply({ embeds: [errorEmbed] });
     }
 }
 
 module.exports = {
     name: "play",
     description: "Play a song from a name or link",
-    permissions: "0x0000000000000800",
-    options: [{
-        name: 'name',
-        description: 'Enter song name / link or playlist',
-        type: ApplicationCommandOptionType.String,
-        required: true
-    }],
-    run: play,
-    queueNames: queueNames,
-    requesters: requesters
+    run: play
 };
