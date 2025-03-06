b735457ccc82dd75384b7afe6a21caf04fe58b44
Cloudie <158478884+hirwko@users.noreply.github.com>
2025-03-06 17:28:10 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ module.exports = {
   language: "en",  // Set the language if needed
   ownerID: ["1106711955563098152", ""],  // Add owner ID(s)
   mongodbUri: "mongodb+srv://tatsumihirako:Revel001@discordbot.wdmjg.mongodb.net/?retryWrites=true&w=majority",  // Your MongoDB connection URI
+  spotifyClientId : "44da772619af47c198d7b66f039b2e05",
+  spotifyClientSecret : "2c4c7cd64fe74b3fb096aa85d7282d56",
   setupFilePath: './commands/setup.json',  // Path to setup file
   commandsDir: './commands',  // Directory where your commands are stored
   embedColor: "#1db954",  // Embed color
